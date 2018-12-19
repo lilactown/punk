fab3a0d0ce44b75b12665a69e265b674051c3d15
Will Acton <will@willacton.com>
2018-12-18 21:06:39 -0800

    setting up app state

--- src/punk/core.cljs
+++ src/punk/core.cljs
@@ -3,7 +3,7 @@
             [clojure.string :as s]
             [clojure.datafy :as d]
             [clojure.core.protocols :as p]
-            [frame.core]))
+            [frame.core :as f]))
 
 (def dbg> (partial js/console.log "punk>"))
 
@@ -51,3 +51,72 @@
 
   default
   (with-index [x] x))
+
+;;
+;; App state
+;;
+
+(defonce punk-db (atom {:entries []
+                        :history []
+                        :current nil
+                        :next {:coll nil
+                               :k nil
+                               :v nil}}))
+
+(defonce punk-frame (f/create-frame
+                    (f/inject-cofx :db)))
+
+(f/reg-cofx
+ punk-frame :db
+ (fn db-cofx [cofx]
+   (assoc cofx :db @punk-db)))
+
+(f/reg-fx
+ punk-frame :db
+ (fn db-fx [v]
+   (when (not (identical? @punk-db v))
+     (reset! punk-db v))))
+
+(defn dbg [f]
+  (fn [x]
+    (f x)
+    x))
+
+(def debug
+  (frame.interceptors/->interceptor
+   :id :punk/debug
+   :before (dbg (partial js/console.log "before> "))
+   :after (dbg (partial js/console.log "after> "))))
+
+
+;;
+;; Punk events
+;;
+
+(f/reg-event-fx
+ punk-frame :punk/add-entry
+ [debug]
+ (fn [cofx [_ x]]
+   {:db (update (:db cofx) :entries conj x)}))
+
+#_(f/dispatch punk-frame [:punk/add-entry "foo"])
+
+;;
+;; External events and subscriptions
+;;
+
+(defn tap-fn [x] (f/dispatch punk-frame [:punk/add-entry x]))
+
+(defn add-taps! []
+  (add-tap tap-fn))
+
+(defn remove-taps! []
+  (remove-tap tap-fn))
+
+#_(add-taps!)
+
+#_(tap> 2)
+
+;;
+;; Connection adapter
+;;
