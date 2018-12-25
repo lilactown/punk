db4ea4b907c2598054fa24ef90e289a3114ebe9b
Will Acton <will@willacton.com>
2018-12-25 15:45:36 -0800

    MapView uses Table

--- src/punk/ui/views.cljs
+++ src/punk/ui/views.cljs
@@ -3,23 +3,11 @@
             [punk.ui.components :as pc]))
 
 (defnc MapView [{:keys [data nav] :as props}]
-  [:div (dissoc props :nav :data)
-   [:<>
-    [:div {:style {:display "flex"
-                   :border-bottom "1px solid #999"
-                   :padding-bottom "3px"
-                   :margin-bottom "3px"}}
-     [:div {:style {:flex 1}} "key"]
-     [:div {:style {:flex 2}} "value"]]
-    (for [[key v] data]
-      [:div {:style {:display "flex"}
-             :key key
-             :class "item"
-             :on-click #(nav data key v)}
-       [:div {:style {:flex 1}}
-        (prn-str key)]
-       [:div {:style {:flex 2}}
-        (prn-str v)]])]])
+  [pc/Table (merge props
+                   {:cols [[:key first {:flex 1}]
+                           [:value second {:flex 3}]]
+                    :on-entry-click (fn [key] (nav data key (get data key)))
+                    :data data})])
 
 (defnc CollView [{:keys [data nav] :as props}]
   [pc/Table (merge props
