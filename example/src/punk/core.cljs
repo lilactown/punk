123a964f72c1801818bc6ba7c7a33d12b9170af7
Will Acton <will@willacton.com>
2019-01-04 14:05:15 -0800

    example

---
+++ example/src/punk/core.cljs
@@ -0,0 +1,124 @@
+(ns punk.core
+  (:require [goog.object :as gobj]
+            [clojure.string :as s]
+            [clojure.datafy :as d]
+            [clojure.core.protocols :as p]
+            [frame.core :as f]
+            [clojure.datafy :as d]))
+
+(def dbg> (partial js/console.log "punk>"))
+
+;;
+;; Implement general protocols
+;;
+
+(extend-protocol IPrintWithWriter
+  js/Symbol
+  (-pr-writer [sym writer _]
+    (-write writer (str "\"" (.toString sym) "\""))))
+
+(defn dataficate [x]
+  (cond
+    ;; (object? x)
+    ;; (do (specify! x
+    ;;       p/Datafiable
+    ;;       (datafy [o] (dissoc (js->clj o)
+    ;;                           ;; lol gross
+    ;;                           "clojure$core$protocols$Datafiable$"
+    ;;                           "clojure$core$protocols$Datafiable$datafy$arity$1")))
+    ;;     x)
+
+    :else x))
+
+;;
+;; State
+;;
+
+(defonce db (atom {:entries []}))
+
+(defonce frame (f/create-frame (f/inject-cofx :db)))
+
+(defonce dispatch #(f/dispatch frame %))
+
+(f/reg-cofx
+ frame :db
+ (fn db-cofx [cofx]
+   (assoc cofx :db @db)))
+
+(f/reg-fx
+ frame :db
+ (fn db-fx [v]
+   (when (not (identical? @db v))
+     (reset! db v))))
+
+(defn dbg [f]
+  (fn [x]
+    (f x)
+    x))
+
+(def debug-db
+  (frame.interceptors/->interceptor
+   :id :punk/debug-db
+   :before (dbg (fn [x] (js/console.log "db/before> " (-> x :coeffects :db))))
+   :after (dbg (fn [x] (js/console.log "db/after> " (-> x :effects :db))))))
+
+(def debug-event
+  (frame.interceptors/->interceptor
+   :id :punk/debug-event
+   :before (dbg (fn [x] (js/console.log "event> " (-> x :coeffects :event))))))
+
+(def debug-fx
+  (frame.interceptors/->interceptor
+   :id :punk/debug-event
+   :after (dbg (fn [x] (js/console.log "effects> " (-> x :effects))))))
+
+(f/reg-event-fx
+ frame :tap
+ [debug-event debug-fx]
+ (fn [{:keys [db]} [_ x]]
+   (let [db' (update db :entries conj x)
+         idx (count (:entries db))
+         dx (d/datafy x)]
+     {:db db'
+      :emit [:entry idx {:value dx
+                         :meta (meta dx)}]})))
+
+(f/reg-event-fx
+ frame :list
+ [debug-event debug-fx]
+ (fn [{:keys [db]} [_ x]]
+   {:emit [:entries (-> (:entries db)
+                        (mapv d/datafy)
+                        (mapv (fn [dx] {:value dx
+                                        :meta (meta dx)})))]}))
+
+(f/reg-event-fx
+ frame :nav
+ [debug-event debug-fx]
+ (fn [{:keys [db]} [_ idx k v]]
+   (let [x (get-in db [:entries idx])
+         ;; nav to next item in datafied object
+         x' (d/nav (d/datafy x) k v)
+         ;; store this nav'd value in db for reference later
+         db' (update db :entries conj x')
+         idx' (count (:entries db))
+         dx' (d/datafy x')]
+     {:db db'
+      :emit [:nav idx {:value dx'
+                       :meta (meta dx')
+                       :idx idx'}]})))
+
+(f/reg-event-fx
+ frame :clear
+ [debug-event debug-fx]
+ (fn [{:keys [db]} _]
+   {:db (assoc db :entries [])}))
+
+(defonce tap-fn (fn tap-fn [x] (dispatch [:tap (dataficate x)])))
+
+(defn remove-taps! []
+  (remove-tap tap-fn))
+
+(defn add-taps! []
+  (remove-taps!)
+  (add-tap tap-fn))
---
+++ example/src/punk_example/core.cljs
@@ -0,0 +1,6 @@
+(ns punk-example.core
+  (:require [punk.adapter.web :as punk]
+            [goog.object :as gobj]
+            [clojure.core.async :as a]))
+
+(punk/start)
