794da9e536c4d1b3ba563d6573f426cf75950058
Will Acton <will@willacton.com>
2019-01-10 14:40:49 -0800

    working on connection status ui

--- ui/src/punk/ui/remote.cljs
+++ ui/src/punk/ui/remote.cljs
@@ -2,54 +2,76 @@
   (:require [punk.ui.core :as core]
             [clojure.core.async :as a]
             [cljs.tools.reader.edn :as edn]
-            [hx.react :as hx]))
+            [hx.react :as hx :refer [defnc]]
+            [hx.react.hooks :as hooks :refer [<-deref]]
+            ["react-dom" :as react-dom]))
 
-(defonce ^:dynamic *conn* (atom nil))
+(defonce state (atom {:status :closed}))
 
-(defonce in-chan (a/chan))
+(defn connect [{:keys [port]}]
+  (let [conn (js/WebSocket. "ws://localhost:9876/ws")
+        subscriber (atom nil)
+        in-chan (a/chan)
+        out-chan (a/chan)]
+    ;; subscriber loop
+    (a/go-loop []
+      (let [ev (a/<! in-chan)]
+        (@subscriber ev)
+        (recur)))
 
-(defonce out-chan (a/chan))
+    ;; send socket messages
+    (a/go-loop []
+      (let [ev (a/<! out-chan)]
+        (.send conn ev)
+        (recur)))
 
-(defonce subscriber (atom nil))
-
-(defonce subscriber-loop
-  (a/go-loop []
-    (let [ev (a/<! in-chan)]
-      (@subscriber ev)
-      (recur))))
-
-(defonce event-loop
-  (a/go-loop []
-    (let [ev (a/<! out-chan)]
-      (.send @*conn* ev)
-      (recur))))
-
-(defn connect []
-  (let [conn (js/WebSocket. "ws://localhost:9876/ws")]
+    ;; websocket config
     (.addEventListener conn "open"
                        (fn [ev]
-                         (println "Opened!")))
+                         (println "Open ")
+                         (swap! state assoc :status :open)))
     (.addEventListener conn "message"
                        (fn [ev]
                          (js/console.log (.-data ev))
                          (a/put! in-chan (.-data ev))))
     (.addEventListener conn "close"
                        (fn [ev]
-                         (println "Closed!")))
-    conn))
+                         (swap! state assoc :status :closed)))
+    (swap! state merge {:in-chan in-chan
+                        :out-chan out-chan
+                        :subscriber subscriber
+                        :conn conn})))
 
-(def in-stream #js {:subscribe #(reset! subscriber %)
-                    :unsubscribe #(reset! subscriber %)})
+(defn close [conn]
+  (when conn
+    (.close conn)))
 
-(def out-stream #js {:put (fn [v] (a/put! out-chan v))})
+(defnc Connection [_]
+  (let [state (<-deref state)]
+    (println "render")
+    [:div {:style {:background-color "#eee"
+                   :padding "10px"}}
+     (prn-str state)]))
+
 
 (defn start! []
   (println "Starting!")
-  (when @*conn* (.close @*conn*))
-  (reset! *conn* (connect))
-  (let [container (or (. js/document getElementById "punk")
+  (close (:conn @state))
+  (let [container (or (. js/document getElementById "connection")
                       (let [new-container (. js/document createElement "div")]
-                        (. new-container setAttribute "id" "punk")
+                        (. new-container setAttribute "id" "connection")
                         (-> js/document .-body (.appendChild new-container))
                         new-container))]
-    (core/start! container in-stream out-stream (prn-str {:drawer? false}))))
+    (react-dom/render (hx/f [Connection]) container))
+  (connect {:port 9876})
+  ;; (let [container (or (. js/document getElementById "punk")
+  ;;                     (let [new-container (. js/document createElement "div")]
+  ;;                       (. new-container setAttribute "id" "punk")
+  ;;                       (-> js/document .-body (.appendChild new-container))
+  ;;                       new-container))
+  ;;       in-stream #js {:subscribe #(swap! state update :subscriber reset! %)
+  ;;                      :unsubscribe #(swap! state update :subscriber reset! nil)}
+
+  ;;       out-stream #js {:put (fn [v] (a/put! (:out-chan @state) v))}]
+  ;;   (core/start! container in-stream out-stream (prn-str {:drawer? false})))
+  )
