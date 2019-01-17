a712ea6fa44738a24564724174cc5968054db38f
Will Acton <will@willacton.com>
2019-01-17 10:30:53 -0800

    project.clj for node-adapter

---
+++ adapter-node/project.clj
@@ -0,0 +1,7 @@
+(defproject lilactown/punk-adapter-node "0.0.2-SNAPSHOT"
+  :description "Punk node application adapter"
+  :url "https://github.com/Lokeh/punk"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :source-paths ["src"]
+  :dependencies [[lilactown/punk-core "0.0.1"]])
