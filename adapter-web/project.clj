1fb17744ab175e98623b827e67a9687108e1b937
Will Acton <will@willacton.com>
2019-02-01 16:51:39 -0800

    0.0.2

--- adapter-web/project.clj
+++ adapter-web/project.clj
@@ -1,7 +1,7 @@
-(defproject lilactown/punk-adapter-web "0.0.2-SNAPSHOT"
+(defproject lilactown/punk-adapter-web "0.0.2"
   :description "Punk web application adapter"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
             :url "https://opensource.org/licenses/EPL-2.0"}
   :source-paths ["src"]
-  :dependencies [[lilactown/punk-core "0.0.2-SNAPSHOT"]])
+  :dependencies [[lilactown/punk-core "0.0.2"]])
--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject lilactown/punk-core "0.0.2-SNAPSHOT"
+(defproject lilactown/punk-core "0.0.2"
   :description "Punk application server core"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
