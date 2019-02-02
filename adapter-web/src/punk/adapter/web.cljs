95a780a72f0ada8feaab37a30512bfb6bf379a8d
Will Acton <will@willacton.com>
2019-02-02 14:53:02 -0800

    bump versions in adapters

--- adapter-web/src/punk/adapter/web.cljs
+++ adapter-web/src/punk/adapter/web.cljs
@@ -48,14 +48,14 @@
 ;;
 
 (def default-script
-  "https://cdn.jsdelivr.net/gh/Lokeh/punk@v0.0.2-alpha.10/ui/dist/js/main.js"
+  "https://cdn.jsdelivr.net/gh/Lokeh/punk@v0.0.3/ui/dist/js/main.js"
   ;; "http://localhost:8701/main.js"
   )
 
 (def default-css
   ["https://fonts.googleapis.com/css?family=Source+Sans+Pro"
-   "https://cdn.jsdelivr.net/gh/Lokeh/punk@v0.0.2-alpha.10/ui/dist/css/grid-layout.css"
-   "https://cdn.jsdelivr.net/gh/Lokeh/punk@v0.0.2-alpha.10/ui/dist/css/resizable.css"])
+   "https://cdn.jsdelivr.net/gh/Lokeh/punk@v0.0.3/ui/dist/css/grid-layout.css"
+   "https://cdn.jsdelivr.net/gh/Lokeh/punk@v0.0.3/ui/dist/css/resizable.css"])
 
 (defn ^{:export true}
   start
