f28d0a7099c75a16534857c76d1f81a6e43a4169
Will Acton <will@willacton.com>
2019-02-10 10:40:36 -0800

    pane styles

--- ui/src/punk/ui/components.cljs
+++ ui/src/punk/ui/components.cljs
@@ -7,30 +7,14 @@
 
 (defnc Pane [{:keys [title id controls children]}]
   [:div {:id id
-         :style {:border "1px solid #ddd"
-                 :box-shadow "2px 2px 1px 1px #eee"
-                 :height "100%"
-                 :background "white"
-                 :position "relative"
-                 :display "flex"
-                 :flex-direction "column"}}
-   [:div {:style {:overflow "auto"
-                  :flex 1}}
-    [:div {:style {:background "#eee"
-                   :padding "8px"
-                   :position "sticky"
-                   :top 0
-                   :z-index "2"
-                   :cursor "move"}
-           :class "titlebar"}
-     [:span {:style {:font-size "1.17em"
-                     :font-weight "500"}}
-      title]]
-    [:div {:style {:padding "8px"}}
+         :class "punk__pane__container"}
+   [:div {:class "punk__pane__body-container"}
+    [:div {:class "punk__pane__titlebar"}
+     [:span title]]
+    [:div {:class "punk__pane__body"}
      children]]
    (when controls
-     [:div {:style {:padding "5px 8px"
-                    :background "#eee"}}
+     [:div {:class "punk__pane__bottom-controls"}
       controls])])
 
 (defnc Table [{:keys [cols data
