c01c782056955a507cd8c77a5d569ee9e17854de
Will Acton <will@willacton.com>
2019-02-13 16:17:48 -0800

    overflow auto in table keys and values

--- ui/src/punk/ui/views.cljs
+++ ui/src/punk/ui/views.cljs
@@ -4,8 +4,8 @@
 
 (defnc MapView [{:keys [data nav] :as props}]
   [pc/Table (merge props
-                   {:cols [[:key first [:div {:style {:flex 1}}]]
-                           [:value second [:div {:style {:flex 3}}]]]
+                   {:cols [[:key first [:div {:style {:flex 1 :overflow "auto"}}]]
+                           [:value second [:div {:style {:flex 3 :overflow "auto"}}]]]
                     :on-entry-click (fn [key] (nav data key (get data key)))
                     :data data}
                    ;; remove nav from props
@@ -13,15 +13,15 @@
 
 (defnc CollView [{:keys [data nav] :as props}]
   [pc/Table (merge props
-                   {:cols [[:idx first [:div {:style {:flex 1}}]]
-                           [:value second [:div {:style {:flex 11}}]]]
+                   {:cols [[:idx first [:div {:style {:flex 1 :overflow "auto"}}]]
+                           [:value second [:div {:style {:flex 11 :overflow "auto"}}]]]
                     :on-entry-click (fn [key] (nav data key (get data key)))
                     :data (map-indexed vector data)}
                    {:nav nil})])
 
 (defnc SetView [{:keys [data nav] :as props}]
   [pc/Table (merge props
-                   {:cols [[:value identity [:div]]]
+                   {:cols [[:value identity [:div {:overflow "auto"}]]]
                     :on-entry-click (fn [v] (nav data nil v))
                     :data data}
                    {:nav nil})])
