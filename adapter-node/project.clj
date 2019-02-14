56f8d7070ded453abc46f8026769ccbfaef1de68
Will Acton <will@willacton.com>
2019-02-13 16:26:00 -0800

    project.clj

--- adapter-node/project.clj
+++ adapter-node/project.clj
@@ -1,4 +1,4 @@
-(defproject lilactown/punk-adapter-node "0.0.6"
+(defproject lilactown/punk-adapter-node "0.0.7"
   :description "Punk node application adapter"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
--- adapter-web/project.clj
+++ adapter-web/project.clj
@@ -1,4 +1,4 @@
-(defproject lilactown/punk-adapter-web "0.0.6"
+(defproject lilactown/punk-adapter-web "0.0.7"
   :description "Punk web application adapter"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
