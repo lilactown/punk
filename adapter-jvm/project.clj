072f62a46222bf078285937bc76e1e39cd4f285f
Will Acton <will@willacton.com>
2019-02-19 15:39:46 -0800

    adapter-jvm/project.clj

---
+++ adapter-jvm/project.clj
@@ -0,0 +1,7 @@
+(defproject lilactown/punk-adapter-jvm "0.0.7"
+  :description "Punk JVM Clojure application adapter"
+  :url "https://github.com/Lokeh/punk"
+  :license {:name "EPL"
+            :url "https://opensource.org/licenses/EPL-2.0"}
+  :source-paths ["src"]
+  :dependencies [[lilactown/punk-core "0.0.2"]])
