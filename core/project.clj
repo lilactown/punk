0bb03da1f06ded1dc41ccc9ee70fcbcc2d2ee4d0
Will Acton <will@willacton.com>
2019-04-25 09:59:10 -0700

    bump frame dep in core

--- core/project.clj
+++ core/project.clj
@@ -1,7 +1,7 @@
-(defproject lilactown/punk-core "0.0.2"
+(defproject lilactown/punk-core "0.0.3"
   :description "Punk application server core"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
             :url "https://opensource.org/licenses/EPL-2.0"}
   :source-paths ["src"]
-  :dependencies [[lilactown/frame "0.0.1"]])
+  :dependencies [[lilactown/frame "0.0.2"]])
