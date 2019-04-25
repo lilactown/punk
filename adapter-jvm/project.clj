4d2a700de224401ae31d3583bc4528e83df18035
Will Acton <will@willacton.com>
2019-04-25 10:00:43 -0700

    add compojure, aleph and bump punk-core in jvm adapter

--- adapter-jvm/project.clj
+++ adapter-jvm/project.clj
@@ -1,7 +1,10 @@
-(defproject lilactown/punk-adapter-jvm "0.0.7"
+(defproject lilactown/punk-adapter-jvm "0.0.8"
   :description "Punk JVM Clojure application adapter"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
             :url "https://opensource.org/licenses/EPL-2.0"}
   :source-paths ["src"]
-  :dependencies [[lilactown/punk-core "0.0.2"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [lilactown/punk-core "0.0.3"]
+                 [compojure "1.6.1"]
+                 [aleph "0.4.6"]])
