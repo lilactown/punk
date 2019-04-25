341b8cf01d4ba665dc738ac41e08df235afaa423
Will Acton <will@willacton.com>
2019-04-25 09:58:20 -0700

    frame transitive deps

--- frame/project.clj
+++ frame/project.clj
@@ -1,6 +1,8 @@
-(defproject lilactown/frame "0.0.1"
+(defproject lilactown/frame "0.0.2"
   :description "State management"
   :url "https://github.com/Lokeh/punk/tree/master/frame"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.logging "0.4.1"]]
   :source-paths ["src"])
