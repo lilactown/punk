0c6e529429837c00958d41e09fd672f49778dedf
Will Acton <will@willacton.com>
2019-08-02 08:18:49 -0700

    bump

--- adapter-jvm/project.clj
+++ adapter-jvm/project.clj
@@ -1,10 +1,10 @@
-(defproject lilactown/punk-adapter-jvm "0.0.10"
+(defproject lilactown/punk-adapter-jvm "0.0.11"
   :description "Punk JVM Clojure application adapter"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
             :url "https://opensource.org/licenses/EPL-2.0"}
   :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [lilactown/punk-core "0.0.5"]
+                 [lilactown/punk-core "0.0.6"]
                  [compojure "1.6.1"]
                  [aleph "0.4.6"]])
--- core/project.clj
+++ core/project.clj
@@ -1,7 +1,7 @@
-(defproject lilactown/punk-core "0.0.5"
+(defproject lilactown/punk-core "0.0.6"
   :description "Punk application server core"
   :url "https://github.com/Lokeh/punk"
   :license {:name "EPL"
             :url "https://opensource.org/licenses/EPL-2.0"}
   :source-paths ["src"]
-  :dependencies [[lilactown/frame "0.0.4"]])
+  :dependencies [[lilactown/frame "0.0.5"]])
